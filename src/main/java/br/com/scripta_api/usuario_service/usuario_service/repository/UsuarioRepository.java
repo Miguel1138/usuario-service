9f7d86a5e3e2ed28e6cf5fa657f0d52fa6f6dbae
Miguel S. da Silva <miguelss616@outlook.com.br>
2025-11-02 12:00:42 -0300

    feat: adicionar método para listar todos os usuários no repositório
    Inclui o método `findAll` em `UsuarioRepository` com transformação de entidades para o domínio utilizando o `UsuarioMapper`.
    
    Signed-off-by: Miguel S. da Silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/usuario_service/usuario_service/repository/UsuarioRepository.java
+++ src/main/java/br/com/scripta_api/usuario_service/usuario_service/repository/UsuarioRepository.java
@@ -6,7 +6,9 @@ import br.com.scripta_api.usuario_service.usuario_service.repository.mapper.Usua
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,6 +26,12 @@ public class UsuarioRepository {
         UsuarioEntity entity = mapper.toEntity(usuarioDomain);
         UsuarioEntity savedEntity = repository.save(entity);
         return mapper.toDomain(savedEntity);
+    }
+
+    public List<Usuario> findAll() {
+        return repository.findAll()
+                .stream().map(mapper::toDomain)
+                .collect(Collectors.toList());
     }
 
 }
