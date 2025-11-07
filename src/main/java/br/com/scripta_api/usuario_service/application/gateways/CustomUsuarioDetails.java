2aadf350c5969262abd98960b9adac98ff24cc7d
Miguel S. da Silva <miguelss616@outlook.com.br>
2025-11-07 20:45:25 -0300

    feat: Cria classe nova para implementar o UserDetails
    - Removido do Entity
    - Mantêm o POJO de Usuario
    
    Signed-off-by: Miguel S. da Silva <miguelss616@outlook.com.br>

---
+++ src/main/java/br/com/scripta_api/usuario_service/application/gateways/CustomUsuarioDetails.java
@@ -0,0 +1,54 @@
+package br.com.scripta_api.usuario_service.application.gateways;
+
+import br.com.scripta_api.usuario_service.application.domain.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUsuarioDetails extends Usuario implements UserDetails {
+    private Usuario usuario;
+
+    public CustomUsuarioDetails(Usuario usuario) {
+        super(usuario);
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String role = "ROLE_" + getTipoDeConta().name();
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.usuario.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.usuario.getMatricula();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
