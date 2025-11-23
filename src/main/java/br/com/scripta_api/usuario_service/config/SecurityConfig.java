104b9edb1d3c9ca9e60bf9504f56ed4df76cab25
miguel.silva <miguelss616@outlook.com.br>
2025-11-23 15:23:17 -0300

    feat: adiciona cors para acesso ao front.
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/usuario_service/config/SecurityConfig.java
+++ src/main/java/br/com/scripta_api/usuario_service/config/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 // 2. Definir a política de sessão como STATELESS (API não guarda sessão)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -58,6 +65,24 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    /**
+     * Efetua liberação para qualquer tipo de origem, não é legal para prod, mas para fins acadêmicos é ok.
+     *
+     * @author miguel.silva
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
